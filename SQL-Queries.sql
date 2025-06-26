c41da60a2625a7528759f0e1eb9c14b0c1b2b8a4
ShivaniMasadi05 <masadisai.shivani@b2btesters.com>
2025-06-26 11:31:07 +0530

    Delete SQL-Queries.sql

--- SQL-Queries.sql
+++
@@ -1,8 +0,0 @@
-## SQL Queries
-The project includes various SQL queries for retrieving information from the database.
-
-- To get all records from respective tables(Students,Subjects,Teachers,Enrollments)
-  ```sql
-  SELECT * FROM Employees;
-  SELECT * FROM Departments;
-  ```
