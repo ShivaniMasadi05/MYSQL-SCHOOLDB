f262439db39c48e4f87568606c45eff4451191a7
ShivaniMasadi05 <masadisai.shivani@b2btesters.com>
2025-06-26 11:29:37 +0530

    Update SQL-Queries.sql

--- SQL-Queries.sql
+++ SQL-Queries.sql
@@ -1,1 +1,8 @@
+## SQL Queries
+The project includes various SQL queries for retrieving information from the database.
 
+- To get all records from respective tables(Students,Subjects,Teachers,Enrollments)
+  ```sql
+  SELECT * FROM Employees;
+  SELECT * FROM Departments;
+  ```
